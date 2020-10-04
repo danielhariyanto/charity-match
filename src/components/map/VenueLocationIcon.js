35ceb77f8548246c26bb363ffb68c3229d4d2af7
sophia wang <sophiawang@sophias-air.dyn.brandeis.edu>
2020-10-03 22:05:20 -0400

    changed marker color + Quiz

--- src/components/map/VenueLocationIcon.js
+++ src/components/map/VenueLocationIcon.js
@@ -7,6 +7,6 @@ export const VenueLocationIcon = L.icon({
   shadowUrl: null,
   shadowSize: null,
   shadowAnchor: null,
-  iconSize: [35, 35],
+  iconSize: [18, 18],
   className: 'leaflet-venue-icon'
 });
