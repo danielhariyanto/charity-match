af3a3a84759f815fdb068c265697a43a6d98cb46
DanielinDenial <55673873+DanielinDenial@users.noreply.github.com>
2020-10-03 23:34:05 -0400

    fixed minors issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MapView from './components/map/MapView';
 import MapSelector from './components/mapselector/MapSelector';
 import Quiz from './components/quiz/Quiz';
-import Navigation from './components/navigation/Navigation';
 import './App.css';
 import styles from './App.module.css';
 //import { BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -10,12 +9,7 @@ import mapselector from './components/mapselector/MapSelector';
 
 class App extends React.Component {
   render(){
-<<<<<<< HEAD
-    return ( 
-      
-=======
-    return (
->>>>>>> baf9314793d83f197751f21c97bd3fdeef666d5d
+    return(
       <div className={styles.container}>
         <Quiz />
         <MapSelector />
